507a67b08aed69724ed76e6fb75039b02ac8f180
Your Name <leonardo_foz@hotmail.com>
2023-03-30 16:41:17 -0300

    Configurado e rodando video no tiktok clone

---
+++ src/pages/Video.js
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from "react";
+import "./Video.css";
+
+function Video() {
+  const videoRef = useRef(null); /* variavel */
+  const [play, setPlay] = useState(false);
+
+  function handdleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  }
+
+  return (
+    <div className="video">
+      Eu sou um componente Video
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handdleStart}
+        loop
+        src="https://firebasestorage.googleapis.com/v0/b/jornada-b5e9d.appspot.com/o/brecker2.mp4?alt=media&token=9aa979e9-0907-4093-abd5-a71efe4fc946"
+      ></video>
+    </div>
+  );
+}
+
+export default Video;
